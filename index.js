71e684d3f3a6936ec4f2e243e84f140df0dbbf98
lssdg <lssdannygiftson@gmail.com>
2023-01-16 19:42:29 +0530

    js file

---
+++ index.js
@@ -0,0 +1,8 @@
+const headerTitle = document.getElementById("header-title");
+const header = document.getElementById("main-header");
+headerTitle.textContent = "Hello";
+headerTitle.innerText="Hi";
+header.style.borderBottom="solid 3px #000";
+const header2 = document.getElementsByClassName("title");
+header2.style.fontWeight="bold";
+header2.style.textColor="green";
