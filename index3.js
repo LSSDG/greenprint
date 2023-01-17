555cd29c04b2c272981622f92c77bf5c802edfe9
lssdg <lssdannygiftson@gmail.com>
2023-01-17 11:24:43 +0530

    assignment5

---
+++ index3.js
@@ -0,0 +1,4 @@
+const items = document.getElementsByTagName('li');
+const item5 = document.getElementsByClassName('new-item');
+items.style.backgroundColor='blue';
+item5.style.textContent="New item";
