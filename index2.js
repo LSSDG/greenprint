067a59ef8f4bd50e0cdf30d478f877e5960b534d
lssdg <lssdannygiftson@gmail.com>
2023-01-17 09:32:39 +0530

    assignment4

---
+++ index2.js
@@ -0,0 +1,4 @@
+const items = document.getElementsByClassName("list-group-item");
+const groupItems = document.getElementsByClassName("list-group");
+items[2].style.backgroundColor="green";
+groupItems.style.fontWeight= 'bold';
