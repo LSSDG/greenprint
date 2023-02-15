c41f265c4cf4721146c55fef317aab196fb10f47
lssdg <lssdannygiftson@gmail.com>
2023-02-15 20:17:21 +0530

    props

--- ReactLearn/expenseapp/src/App.js
+++ ReactLearn/expenseapp/src/App.js
@@ -2,10 +2,19 @@ import ExpenseItem from './components/ExpenseItem';
 
 
 function App() {
+  const expenses = [{title:'Car',price:4000,date:new Date(2022,1,12),loc:'City1'},
+  {title:'CarB',price:4500,date:new Date(2022,4,21),loc:'City2'},
+  {title:'CarC',price:7000,date:new Date(2022,5,5),loc:'City3'},
+  {title:'CarD',price:6000,date:new Date(2022,7,12),loc:'City4'}];
+  //{for(let i=0;i<expenses.length;i++){
   return (
     <div className="App">
-      <ExpenseItem></ExpenseItem>
       
+      <ExpenseItem expense={expenses[0]}></ExpenseItem>
+      <ExpenseItem expense={expenses[1]}></ExpenseItem>
+      <ExpenseItem expense={expenses[2]}></ExpenseItem>
+      <ExpenseItem expense={expenses[3]}></ExpenseItem>
+    
     </div>
   );
 }
--- ReactLearn/expenseapp/src/components/ExpenseItem.js
+++ ReactLearn/expenseapp/src/components/ExpenseItem.js
@@ -1,10 +1,17 @@
-function ExpenseItem(){
+import './ExpenseItem.css';
+function ExpenseItem(props){
+    const expenseDate= new Date(2022,8,1);
+    const expenseTitle= 'Car Purchase';
+    const expensePrice = 10000;
+    const locationOfExpenditure='City';
     return(
-        <div>
-            <h1>Expense Items</h1>
-            <h2>Food Rs10</h2>
-            <h2>Petrol Rs100</h2>
-            <h2>Movies Rs200</h2>
+        <div className="expense-item">
+            <div>{props.expense.date.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{props.expense.title}</h2>
+                <div className='expense-item__price'>Rs{props.expense.price}</div>
+                <div>{props.expense.loc}</div>
+            </div>
         </div>
     )
 
