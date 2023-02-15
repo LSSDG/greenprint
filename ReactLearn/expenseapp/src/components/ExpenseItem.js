f337bf26c9175fee6188fa3dff7997d7ddcd6b1a
lssdg <lssdannygiftson@gmail.com>
2023-02-15 22:50:24 +0530

    extracomponents

--- ReactLearn/expenseapp/src/components/ExpenseItem.js
+++ ReactLearn/expenseapp/src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 function ExpenseItem(props){
     const expenseDate= new Date(2022,8,1);
     const expenseTitle= 'Car Purchase';
     const expensePrice = 10000;
-    const locationOfExpenditure='City';
+    
     return(
         <div className="expense-item">
-            <div>{props.expense.date.toISOString()}</div>
-            <div className="expense-item__description">
-                <h2>{props.expense.title}</h2>
-                <div className='expense-item__price'>Rs{props.expense.price}</div>
-                <div>{props.expense.loc}</div>
-            </div>
+            <ExpenseDate date={props.expense.date}/>
+            
+            <ExpenseDetails title={props.expense.title} price={props.expense.price} loc={props.expense.loc}/>
+                
+            
         </div>
     )
 
