7ce1eba5360540be9047109aa632cf7a48d0c239
lssdg <lssdannygiftson@gmail.com>
2023-02-15 17:16:43 +0530

    reactexpense

---
+++ ReactLearn/expenseapp/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem(){
+    return(
+        <div>
+            <h1>Expense Items</h1>
+            <h2>Food Rs10</h2>
+            <h2>Petrol Rs100</h2>
+            <h2>Movies Rs200</h2>
+        </div>
+    )
+
+}
+
+export default ExpenseItem;
