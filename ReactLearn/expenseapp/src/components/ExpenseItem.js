66ac9a1daf2fe9b0546c7916350d77bc5cb43e28
lssdg <lssdannygiftson@gmail.com>
2023-02-16 19:46:42 +0530

    deletebtn

--- ReactLearn/expenseapp/src/components/ExpenseItem.js
+++ ReactLearn/expenseapp/src/components/ExpenseItem.js
@@ -1,17 +1,23 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
+import react ,{useState} from 'react';
 const ExpenseItem=(props)=>{
     const expenseDate= new Date(2022,8,1);
     const expenseTitle= 'Car Purchase';
     const expensePrice = 10000;
+    const [amount,setAmount]=useState(props.expense.price);
+    const deleteExpense = ()=>{
+        setAmount(null);
+
+    }
     
     return(
         <div className="expense-item">
             <ExpenseDate date={props.expense.date}/>
             
-            <ExpenseDetails title={props.expense.title} price={props.expense.price} loc={props.expense.loc}/>
-                
+            <ExpenseDetails title={props.expense.title} price={amount} loc={props.expense.loc}/>
+            <button onClick={deleteExpense} >Delete Expense</button>  
             
         </div>
     )
